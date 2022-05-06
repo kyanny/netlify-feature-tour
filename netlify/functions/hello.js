fcb2fe6ec278322b1d1907e94b70197979967079
Kensuke Nagae <kyanny@gmail.com>
2022-05-06 15:54:48 +0900

    log

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
+console.log(1);
 
 exports.handler = async function (event, context) {
+    console.log(2);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
