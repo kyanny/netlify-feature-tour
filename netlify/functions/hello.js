6b87ca36a6cef1145df49cc5e8529083831c1d57
Kensuke Nagae <kyanny@gmail.com>
2022-05-06 19:12:01 +0900

    jsdom

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,10 +1,16 @@
 import fetch from "node-fetch";
-console.log(1);
 
 exports.handler = async function (event, context) {
-    console.log(2);
+    let body = "";
+    fetch("https://anchor.fm/s/6cb3ad28/podcast/rss")
+        .then(res => res.text())
+        .then(xml => {
+            body = xml;
+            const parser = new DOMParser();
+            return parser.parseFromString(xml, "text/xml");
+        });
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World" + body }),
   };
 }
