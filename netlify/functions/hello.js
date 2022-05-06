9b36b4e0ac09b5d6e7fce63b397ca187d2e642e1
Kensuke Nagae <kyanny@gmail.com>
2022-05-06 15:25:39 +0900

    hello func

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,6 @@
-exports.handler = async function(event, context) {
-    // your server-side functionality
-    return "Hello";
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
+  };
 }
